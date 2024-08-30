9595030b425e1eb29ea4066fde6a9246810d2779
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-08-30 21:27:22 +0200

    ✨ Home > Main > Add video background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
+"use client";
+import styled from 'styled-components';
+
+// Définissez le composant StyledBackground avec les styles pour la vidéo en fond
+const StyledBackground = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center; // Centre horizontalement
+  align-items: center; // Centre verticalement
+  text-align: center; // Centre le texte à l'intérieur des éléments
+
+  video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -1; // Assurez-vous que la vidéo est en arrière-plan
+  }
+
+  h1, h2 {
+    color: white; // Couleur du texte pour le contraste
+    z-index: 1; // Assurez-vous que le texte est au-dessus de la vidéo
+  }
+
+  h2 {
+    margin-top: 0.5em; // Espacement entre le h1 et le h2
+  }
+`;
+
 export default function Home() {
     return (
-        <h1>Bienvenue sur mon application !</h1>
-    )
+        <StyledBackground>
+            <div>
+                <h1>MAKEY SIONG</h1>
+                <h2>FILMMAKER</h2>
+            </div>
+            <video autoPlay loop>
+                <source src="/car.mp4" type="video/mp4" />
+            </video>
+        </StyledBackground>
+    );
 }
