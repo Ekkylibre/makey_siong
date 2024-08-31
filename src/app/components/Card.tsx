5bb72449be7bb402c569b07241bcfa60daab8073
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-08-31 16:55:29 +0200

    💄 Components > Card > Change size

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,52 +3,51 @@ import styled from 'styled-components';
 
 // Style pour la carte avec effet de zoom au survol
 const CardContainer = styled.div`
-  width: 300px;
+  width: 400px; // Augmenter la largeur de la carte
   overflow: hidden;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.5); // Ajuster l'ombre pour correspondre à la taille
   font-family: Arial, sans-serif;
   text-align: center;
   background-color: #0e232d;
   color: white;
-  transition: transform 1s ease; // Transition pour l'effet de zoom
+  transition: transform 1s ease;
 
   &:hover {
-    transform: scale(1.05); // Agrandir la carte au survol
+    transform: scale(1.05);
   }
 `;
 
 // Style pour l'image
 const CardImage = styled.img`
   width: 100%;
-  height: 200px;
+  height: 300px; // Augmenter la hauteur de l'image
   object-fit: cover;
 `;
 
 // Style pour le texte
 const CardContent = styled.div`
-  padding: 16px;
+  padding: 24px; // Augmenter le padding interne
 `;
 
 // Style pour le titre
 const CardTitle = styled.h3`
-  margin-bottom: 16px; // Ajustez la valeur pour l'espacement désiré
+  margin-bottom: 24px; // Augmenter l'espacement sous le titre
 `;
 
 // Style pour le bouton
 const CardButton = styled.button`
-  background-color: #0e232d; /* Couleur de fond initiale */
-  color: white; /* Couleur du texte */
-  border: 1px solid white; /* Bordure bleue */
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
+  background-color: #0e232d;
+  color: white;
+  border: 1px solid white;
+  padding: 0.75rem 1.5rem; // Augmenter le padding du bouton
+  font-size: 1.2rem; // Augmenter la taille de la police
   cursor: pointer;
-  transition: filter 0.3s, color 0.3s; /* Transition pour l'effet de luminosité */
-  margin-top: 24px;
+  transition: filter 0.3s, color 0.3s;
+  margin-top: 32px; // Augmenter l'espacement au-dessus du bouton
   
-  /* Applique un léger effet de luminosité */
   &:hover {
-    filter: brightness(1.3); /* Augmente la luminosité de 30% au survol */
-    color: #007bff; /* Change la couleur du texte au survol */
+    filter: brightness(1.3);
+    color: #007bff;
   }
 `;
 
