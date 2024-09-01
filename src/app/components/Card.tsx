c60e9309835303fc597270fceed44a270c4b769e
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-09-01 11:41:51 +0200

    💄 Components > Card > Change size

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import ButtonLink from './ButtonLink';
 
 // Style pour la carte avec effet de zoom au survol
 const CardContainer = styled.div`
-  width: 400px; // Augmenter la largeur de la carte
+  width: 400px; // Largeur fixe de la carte
+  height: 500px; // Hauteur fixe pour la carte
   overflow: hidden;
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.5); // Ajuster l'ombre pour correspondre à la taille
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.5); // Ombre pour correspondre à la taille
   font-family: Arial, sans-serif;
   text-align: center;
   background-color: #0e232d;
@@ -20,36 +22,37 @@ const CardContainer = styled.div`
 // Style pour l'image
 const CardImage = styled.img`
   width: 100%;
-  height: 300px; // Augmenter la hauteur de l'image
+  height: 300px; // Hauteur fixe de l'image
   object-fit: cover;
 `;
 
 // Style pour le texte
 const CardContent = styled.div`
-  padding: 24px; // Augmenter le padding interne
+  padding: 24px; // Padding interne
+  height: calc(100% - 300px); // Hauteur du contenu ajustée en fonction de la hauteur totale et de la hauteur de l'image
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; // Évite que le contenu dépasse de la carte
 `;
 
 // Style pour le titre
 const CardTitle = styled.h3`
-  margin-bottom: 24px; // Augmenter l'espacement sous le titre
+  margin-bottom: 24px; // Espacement sous le titre
 `;
 
-// Style pour le bouton
-const CardButton = styled.button`
-  background-color: #0e232d;
-  color: white;
-  border: 1px solid white;
-  padding: 0.75rem 1.5rem; // Augmenter le padding du bouton
-  font-size: 1.2rem; // Augmenter la taille de la police
-  cursor: pointer;
-  transition: filter 0.3s, color 0.3s;
-  margin-top: 32px; // Augmenter l'espacement au-dessus du bouton
-  
-  &:hover {
-    filter: brightness(1.3);
-    color: #007bff;
-  }
+// Style pour la description
+const Description = styled.p`
+  flex: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3; // Limite à 3 lignes
+  line-height: 1.5em; // Hauteur de ligne pour le texte
+  max-height: 4.5em; // Hauteur maximale basée sur la hauteur de ligne
+  margin: 0; // Assure que le marges ne causent pas de débordement
 `;
+
 
 // Composant Card
 type CardProps = {
@@ -64,9 +67,12 @@ const Card: React.FC<CardProps> = ({ imageSrc, title, description }) => {
       <CardImage src={imageSrc} alt={title} />
       <CardContent>
         <CardTitle>{title}</CardTitle>
-        <p>{description}</p>
-        <CardButton>En savoir plus</CardButton>
+        <Description>{description}</Description>
+        <ButtonLink href="" target="_blank" rel="noopener noreferrer">
+          En savoir plus
+        </ButtonLink>
       </CardContent>
+      
     </CardContainer>
   );
 };
