9fb4796a9ca6a330e675b4132d2b5ae71bb3598c
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-09-01 21:57:32 +0200

    🍱 Theme > Colors > Create colors assets

---
+++ src/app/components/theme/index.tsx
@@ -0,0 +1,7 @@
+export const colors = {
+    primary: "#0e232d", // Couleur principale utilisée dans la Navbar, le MainContent, etc.
+    secondary: "#081319", // Couleur utilisée dans la section Services
+    white: "#ffffff", // Couleur blanche utilisée pour le texte et le soulignement
+    gray: "#666", // Couleur grise utilisée pour la ligne horizontale dans le footer
+  };
+  
