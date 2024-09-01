c44fe5fbfe976a6bfe0afe0871e73ec89099a3f1
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-09-01 22:23:25 +0200

    🍱 Theme > Colors > Rework colors assets

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,7 +1,5 @@
 export const colors = {
-    primary: "#0e232d", // Couleur principale utilisée dans la Navbar, le MainContent, etc.
-    secondary: "#081319", // Couleur utilisée dans la section Services
-    white: "#ffffff", // Couleur blanche utilisée pour le texte et le soulignement
-    gray: "#666", // Couleur grise utilisée pour la ligne horizontale dans le footer
+    primary: "#0e232d",
+    secondary: "#081319",
   };
   
